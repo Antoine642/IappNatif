45514c36c7277702b853e14e304398ba6f9a1322
phothinh <phothinh01@gmail.com>
2024-10-08 15:54:10 +0200

    Initial Commit

---
+++ lappNatif/lappNatifApp.swift
@@ -0,0 +1,17 @@
+//
+//  lappNatifApp.swift
+//  lappNatif
+//
+//  Created by eduadmin on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct lappNatifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
