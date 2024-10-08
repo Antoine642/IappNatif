35d07edaeff46961add8fced24f16ff0cdcc3fdc
phothinh <phothinh01@gmail.com>
2024-10-08 16:27:19 +0200

    first commit

--- lappNatif/ContentView.swift
+++ lappNatif/ContentView.swift
@@ -8,17 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    var loot = ["Epée", "Bouclier", "Armure"]
 
+        var body: some View {
+            List(loot) { item in
+                Text(item)
+            }
+        }
+}
 #Preview {
     ContentView()
 }
